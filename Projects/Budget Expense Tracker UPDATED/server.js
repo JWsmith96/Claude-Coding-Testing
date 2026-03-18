b11806ddda7854cd0aa6d02945d56e5195804cbf
Josephsmith1996 <imjosephsmith@hotmail.com>
2026-03-18 14:33:47 +1100

    feat: add global error handling middleware

--- Projects/Budget Expense Tracker UPDATED/server.js
+++ Projects/Budget Expense Tracker UPDATED/server.js
@@ -42,6 +42,12 @@ app.use(express.urlencoded({ extended: false }));
 // Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Something went wrong. Please try again.');
+});
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
