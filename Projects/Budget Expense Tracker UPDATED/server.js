06d08d49cf38478e1626b43ba7b18051b131fa48
Josephsmith1996 <imjosephsmith@hotmail.com>
2026-03-18 14:32:07 +1100

    feat: set up base Express app with EJS templating and static file serving

---
+++ Projects/Budget Expense Tracker UPDATED/server.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+// View engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+// Body parsing
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
